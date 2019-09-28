1f21426cc00885067a74cb31e8489f6fb2e77ccf
Omar Aflak <aflakomar@gmail.com>
2019-09-28 20:11:16 +0200

    added string command

--- examples/apdu_cli.cpp
+++ examples/apdu_cli.cpp
@@ -16,24 +16,31 @@ int main() {
     APDU apdu;
 
     if(manager.open()){
-        std::cout << "Nfc device opened. Waiting for target..." << std::endl;
+        std::cout << "Nfc device opened. Waiting for target...";
         manager.waitForTarget();
 
-        std::cout << std::endl << "Target detected! Type 'exit' to quit the program." << std::endl << std::endl;
+        std::cout << "Target detected!" << std::endl;
+        std::cout << "Type 'exit' to quit the program." << std::endl;
+        std::cout << "Type 'string' to convert last response to string." << std::endl << std::endl;
 
+        APDU apdu;
         std::string input;
         while(input != "exit"){
             std::cout << ">> ";
             std::getline(std::cin, input);
             spaceless(input);
-            std::vector<uint8_t> cmd = hexStringToByteArray(input);
 
-            APDU apdu;
-            apdu.setAPDU(cmd);
-            if(manager.transceive(apdu)){
-                std::vector<uint8_t> response = apdu.getRespBytes();
-                std::cout << "<< ";
-                print(response);
+            if(input == "string"){
+                std::cout << apdu.getRespString() << std::endl;
+            } else{
+                std::vector<uint8_t> cmd = hexStringToByteArray(input);
+                apdu.setAPDU(cmd);
+
+                if(manager.transceive(apdu)){
+                    std::vector<uint8_t> response = apdu.getRespBytes();
+                    std::cout << "<< ";
+                    print(response);
+                }
             }
         }
 
